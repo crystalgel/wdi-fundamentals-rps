bbe7dcf989247729453bf9edccdd90021ff2f860
Crystal <crystal@CRYSTALs-MacBook-Air-2.local>
2015-09-28 07:18:48 -0700

    unit 4 hw

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -21,22 +21,44 @@ function randomPlay() {
 /*           Write Your Code Below            */
 ////////////////////////////////////////////////
 
-function getPlayerMove(move) {
+function getPlayerMove(move) 
+{
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* Your Expression */;
+  return move || getInput();
 }
 
 function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return /* Your Expression */;
+    return move || randomPlay();
 }
 
 function getWinner(playerMove,computerMove) {
-    var winner;
+    var winner = "tie";
+    if (playerMove == "rock"){
+    	if (computerMove == "paper") {
+    		winner = "computer";
+    	}else if (computerMove == "scissors") {
+    		winner = "player"; 
+    	}
+    }
+    if (playerMove == "paper"){
+    	if (computerMove == "rock") {
+    		winner = "player";
+    	}else if (computerMove == "scissors") {
+    		winner = "computer"; 
+    	}
+    }
+    if (playerMove == "scissors"){
+    	if (computerMove == "paper") {
+    		winner = "player";
+    	}else if (computerMove == "rock") {
+    		winner = "computer"; 
+    	}
+    }
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
