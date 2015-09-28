7e53c9cfc14043637c0598432c303255dd770340
Crystal <crystal@CRYSTALs-MacBook-Air-2.local>
2015-09-28 07:39:14 -0700

    unit 5 hw

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -70,6 +70,18 @@ function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
     var playerWins = 0;
     var computerWins = 0;
+    var play = 1;
+    while (play) {
+    	var winner = getwinner (getPlayerMove(), getComputerMove()); 
+    	if (winner == "player") {
+    		playerWins++;
+    	} else if (winner == "computer") {
+    		computerWins ++;
+    	}
+    	if (playerWins ==5 || computerWins ==5) {
+    		play =0;
+    	} 
+    }
     // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
     /* YOUR CODE HERE */
     return [playerWins, computerWins];
